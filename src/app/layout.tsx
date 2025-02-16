fe533ae8987fb38e2c708c04416a5ec779b2bf82
char502 <char502@hotmail.com>
2025-02-16 12:07:29 +0000

    reusable header components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
-import Link from 'next/link';
-import { homePath, ticketsPath } from '@/paths';
+import { Header } from '@/components/header';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -30,25 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <nav
-          className="
-            supports-backdrop-blur:bg-background/60 
-            fixed left-0 right-0 top-0 z-20 
-            border-b bg-background/95 backdrop-blur 
-            w-full flex py-2.5 px-5 justify-between
-          "
-        >
-          <div>
-            <Link href={homePath()} className="text-lg font-bold">
-              Home
-            </Link>
-          </div>
-          <div>
-            <Link href={ticketsPath()} className="text-sm underline">
-              Tickets
-            </Link>
-          </div>
-        </nav>
+        <Header />
         <main
           className="
             min-h-screen flex-1 
