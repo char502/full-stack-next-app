48b4556fcc8d8863ce7a2795da778a3016228343
char502 <char502@hotmail.com>
2025-03-23 14:46:14 +0000

    dark mode

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,21 +1,36 @@
+import Link from 'next/link';
 import { Placeholder } from '@/components/placeholder';
+import { Button } from '@/components/ui/button';
 import { initialTickets } from '@/data';
+import { TicketItem } from '@/features/ticket/components/ticket-item';
+import { ticketsPath } from '@/paths';
 
 type TicketPageProps = {
-  params: {
+  params: Promise<{
     ticketId: string;
-  };
+  }>;
 };
 
-const TicketPage = ({ params }: TicketPageProps) => {
+const TicketPage = async (props: TicketPageProps) => {
+  const params = await props.params;
   const ticket = initialTickets.find((ticket) => ticket.id === params.ticketId);
 
-  if (!ticket) return <Placeholder label="Ticket not found" />;
+  if (!ticket) {
+    return (
+      <Placeholder
+        label="Ticket not found"
+        button={
+          <Button asChild variant="outline">
+            <Link href={ticketsPath()}>Go to tickets</Link>
+          </Button>
+        }
+      />
+    );
+  }
 
   return (
-    <div>
-      <h2 className="text-lg">{ticket.title}</h2>
-      <p className="text-sm">{ticket.content}</p>
+    <div className="flex justify-center animate-fade-in-from-top">
+      <TicketItem ticket={ticket} isDetail />
     </div>
   );
 };
--- src/components/placeholder.tsx
+++ src/components/placeholder.tsx
@@ -1,11 +1,26 @@
+import { LucideMessageSquareWarning } from 'lucide-react';
+import { cloneElement } from 'react';
+
 type PlaceholderProps = {
   label: string;
+  icon?: React.ReactElement;
+  button?: React.ReactElement;
 };
 
-const Placeholder = ({ label }: PlaceholderProps) => {
+const Placeholder = ({
+  label,
+  icon = <LucideMessageSquareWarning />,
+  button = <div className="h-10" />,
+}: PlaceholderProps) => {
   return (
-    <div className="flex-1 self-center flex flex-col items-center justify-center">
+    <div className="flex-1 self-center flex flex-col items-center justify-center gap-y-2">
+      {cloneElement(icon, {
+        className: 'w-16 h-16',
+      })}
       <h2 className="text-lg text-center">{label}</h2>
+      {cloneElement(button, {
+        className: 'h-10',
+      })}
     </div>
   );
 };
